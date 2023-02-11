a5d5af60c118172999aa9aca640a51a565e83eac
alex-code <1797626+alex-code@users.noreply.github.com>
2023-02-11 15:01:58 +0000

    Fixes

--- lib/UI/Elements/Header.cpp
+++ lib/UI/Elements/Header.cpp
@@ -12,7 +12,7 @@ void Header::draw() {
   
   uint16_t textW = UI::tft->textWidth(_label);
   uint16_t lineY = Element::_y + (UI::tft->fontHeight() / 2);
-  uint16_t lineW = ((Element::_w - textW) / 2) - 10;
+  uint16_t lineW = std::max<int16_t>(((Element::_w - textW) / 2) - 10, 0);
 
   UI::tft->fillRect(Element::_x, lineY, lineW, 2, _color);
   UI::tft->fillRect(Element::_w - lineW, lineY, lineW, 2, _color);
