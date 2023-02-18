ae29e385215258c50e24581be714127be2e97c2f
alex-code <1797626+alex-code@users.noreply.github.com>
2023-02-18 17:29:39 +0000

    Arduino IDE fix for `myFilter`

--- myFilter.cpp
+++ myFilter.cpp
@@ -1,5 +1,9 @@
-#include <Arduino.h>
-#include <CommandDistributor.h>
+#include "Arduino.h"
+#include "CommandDistributor.h"
+
+/*
+ * Adds extra command to your Command Station which can be used by the throttle
+ */
 
 void myFilter(Print *stream, byte &opcode, byte &paramCount, int16_t p[]) {
   (void)stream; // Stops warning
